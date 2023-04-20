ffb8d845e13392715a0ef76d881a28381df34754
Tim Mecklem <timothy@mecklem.com>
2023-04-20 15:13:17 -0400

    Play around with shadow variables and one-line blocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
 
+  attr_accessor :name
+
   private
 
-  def useless
+  def useless(name)
+    name = @name
     puts "Don't call me, I won't call you"
+    self.map do |attr| puts name end
   end
 end
